1090086b683ca4e810c751a45fe2348ed8ca5a7f
Fuad sheerah <mhdshalabystar@gmail.com>
2020-01-30 17:33:34 +0300

    feedreader.js : check each feed has url test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,12 +26,17 @@ $(
         expect(allFeeds).toBeDefined();
         expect(allFeeds.length).not.toBe(0);
       });
-
-      it("each has url", () => {});
       /* TODO: Write a test that loops through each feed
        * in the allFeeds object and ensures it has a URL defined
        * and that the URL is not empty.
        */
+      it("each has url", () => {
+        for (let feed of allFeeds) {
+          expect(feed.url).toBeDefined();
+          expect(feed.url.constructor).toBe(String);
+          expect(feed.url.length).not.toBe(0);
+        }
+      });
 
       /* TODO: Write a test that loops through each feed
        * in the allFeeds object and ensures it has a name defined
