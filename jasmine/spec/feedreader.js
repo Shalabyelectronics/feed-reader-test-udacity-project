965a5d554721a2b197a54fd929eeb96d5ee5f1a9
Fuad sheerah <mhdshalabystar@gmail.com>
2020-02-01 00:46:03 +0300

    feedreader.js: add Intial entries test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -64,6 +64,11 @@ $(
       });
 
       it("toggles view when icon is clicked", () => {
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
         let menuIcon = document.querySelector("a.menu-icon-link");
         menuIcon.click();
         expect(document.body.classList.contains("menu-hidden")).toBe(false);
@@ -72,20 +77,18 @@ $(
       });
     });
 
-    /* TODO: Write a test that ensures the menu changes
-     * visibility when the menu icon is clicked. This test
-     * should have two expectations: does the menu display when
-     * clicked and does it hide when clicked again.
-     */
-
     /* TODO: Write a new test suite named "Initial Entries" */
-
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+    describe("Initial Entries", () => {
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
+      beforeEach(function(done) {
+        loadFeed(1, done);
+      });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
